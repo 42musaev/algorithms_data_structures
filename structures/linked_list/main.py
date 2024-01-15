1b1f70deb565fb109aba61f1b3b1dc38bba1c38a
Askhab Musaev <42musaev@gmail.com>
2024-01-15 22:48:39 +0300

    structure: linked-list|update

--- structures/linked_list/main.py
+++ structures/linked_list/main.py
@@ -1,49 +1,110 @@
-from typing import Any
+from typing import Any, Optional
 
 
 class LinkedList:
-    __head = None
-    __length = 0
-
     class Node:
-        def __init__(self, element: Any):
-            self.element = element
-            self.next_node = None
+        def __init__(self, value: Any, next_node: "Node" = None) -> None:
+            self.value = value
+            self.next_node = next_node
 
-    def __str__(self) -> str:
-        last_node = self.__head
-        pattern = '['
-        if not last_node:
-            return '[]',
-        if last_node and not last_node.next_node:
-            return f'[{last_node.element}]'
-        while last_node.next_node:
-            pattern += str(last_node.element) + ', '
-            last_node = last_node.next_node
-        pattern += f'{last_node.element}]'
-        return pattern
+        def __str__(self):
+            return str(self.value)
 
-    def __getitem__(self, item: int) -> Any:
-        i = 0
-        node = self.__head
-        while i < item:
-            node = node.next_node
-            i += 1
-        if not node:
-            raise IndexError('list index out of range')
-        return node.element
+    def __init__(self) -> None:
+        self.head = None
+        self._size = 0
 
-    def append(self, element: Any) -> Any:
-        last_node = self.__head
-        if not last_node:
-            self.__head = self.Node(element)
-            return element
-        while last_node.next_node:
-            last_node = last_node.next_node
-        last_node.next_node = self.Node(element)
+    def __str__(self):
+        result = ""
+        current_node = self.head
+        while current_node:
+            result += str(current_node.value)
+            if current_node.next_node:
+                result += ", "
+            current_node = current_node.next_node
+
+        return "[" + result + "]"
+
+    def append(self, value: Any) -> None:
+        if not self.head:
+            self.head = self.Node(value)
+            self.size += 1
+            return
+        current_node = self.head
+        while current_node.next_node:
+            current_node = current_node.next_node
+        current_node.next_node = self.Node(value)
+        self.size += 1
+
+    def prepend(self, value: Any):
+        if not self.head:
+            self.head = self.Node(value)
+            self.size += 1
+            return
+        current_node = self.Node(value)
+        current_node.next_node = self.head
+        self.head = current_node
+        self.size += 1
+
+    def insert(self, value: Any, index: int):
+        if index == 0:
+            self.prepend(value)
+        else:
+            _idx = 1
+            current_node = self.head
+            insert_node = self.Node(value)
+            while current_node.next_node:
+                previous_node = current_node
+                current_node = current_node.next_node
+                if index == _idx:
+                    previous_node.next_node = insert_node
+                    insert_node.next_node = current_node
+                    break
+                _idx += 1
+            else:
+                current_node.next_node = insert_node
+
+    def contains(self, value: Any) -> Node | None:
+        if not self.head:
+            return
+        current_node = self.head
+        if current_node.value == value:
+            return current_node
+        while current_node.next_node:
+            if current_node.value == value:
+                return current_node
+            current_node = current_node.next_node
+
+    def remove(self, value: Any):
+        if not self.head:
+            return
+        current_node = self.head
+        if current_node.value == value:
+            self.head = current_node.next_node
+        while current_node.next_node:
+            previous_node = current_node
+            current_node = current_node.next_node
+            if current_node.value == value:
+                previous_node.next_node = current_node.next_node
+                self.size -= 1
+                break
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self._size = value
 
 
-a = LinkedList()
-a.append(1)
-a.append(2)
-a.append(3)
+l = LinkedList()
+for letter in ('c', 'b', 'a'):
+    l.insert(letter, 0)
+
+for letter in ('d', 'f', 'g'):
+    l.append(letter)
+
+print(l.contains('c'))
+print(l)
+print(l.size)
